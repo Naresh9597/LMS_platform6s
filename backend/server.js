eacc0e263c97f622bb10a3fca556cb628d827144
Naresh9597 <2022it0516@svce.ac.in>
2025-10-17 20:37:34 +0530

    Fix backend to serve nested React frontend

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 import path from "path";
 import { fileURLToPath } from "url";
-import express from "express";
+//import express from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
