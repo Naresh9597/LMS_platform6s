d55a61cc2fdc143c01898a32af58d7a020c1431f
Naresh9597 <2022it0516@svce.ac.in>
2025-10-17 21:48:38 +0530

    Fix backend to serve nested React frontend

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(buildPath));
 
   // ⚙️ Express v5 fix: use "/*" instead of "*"
-  app.get("/*", (req, res) => {
+  app.get("/*?", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 }
